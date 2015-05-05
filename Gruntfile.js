e6835aa6035dcac7f849475598afbd228d2433eb
Alexey Petushkov <alexey.petushkov@auriga.com>
2015-05-05 20:08:51 +0300

    Fixed CI

--- Gruntfile.js
+++ Gruntfile.js
@@ -26,6 +26,7 @@ module.exports = function (grunt) {
           main: {
               src: 'test/**/*.js',
               options: {
+                  vendor:  ['node_modules/jasmine-ajax/lib/mock-ajax.js'],
                   helpers: ['lib/jslog.js', 'lib/vendor/lodash.min.js']
               }
           }
