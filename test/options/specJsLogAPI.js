405298b895212e4598d4fee568859db9149bf30c
Alexey Petushkov <alexey.petushkov@auriga.com>
2015-05-07 20:32:13 +0300

    In progress

---
+++ test/options/specJsLogAPI.js
@@ -0,0 +1,55 @@
+describe('have valid API', function () {
+    var /**JsLog*/logger,
+        sampleOptions = {
+            "key": "MY_KEY",
+            "hookConsole": false
+        },
+        sampleData = {
+            "b": 1,
+            "a": 2
+        },
+        sampleError = new Error('Some error text');
+
+    beforeEach(function () {
+        jasmine.Ajax.install();
+        logger = new JsLog(sampleOptions);
+    });
+
+    afterEach(function () {
+        logger = null;
+        jasmine.Ajax.uninstall();
+    });
+
+    it('sending initial info matches protocol', function () {
+        var spy = logger.sendToServer = jasmine.createSpy('sendToServer');
+        logger.systemInfo(false);
+        expect(spy).toHaveBeenCalled();
+        expect(spy.calls.count()).toEqual(1);
+        expect(spy.calls.mostRecent().args[0]).toEqual(sampleOptions.key);
+        expect(spy.calls.mostRecent().args[1]).toEqual('systemInfo');
+    });
+
+    it('sending log matches protocol', function () {
+        var spy = logger.sendToServer = jasmine.createSpy('sendToServer');
+        logger.log(sampleData);
+        expect(spy).toHaveBeenCalled();
+        expect(spy.calls.count()).toEqual(1);
+        expect(spy.calls.mostRecent().args[0]).toEqual(sampleOptions.key);
+        expect(spy.calls.mostRecent().args[1]).toEqual('log');
+        expect(spy.calls.mostRecent().args[2]).toEqual(sampleData);
+    });
+
+    it('sending error matches protocol', function () {
+        var spy = logger.sendToServer = jasmine.createSpy('sendToServer'),
+            extractedException = logger.exceptionToObject(sampleError);
+        logger.error(sampleError);
+        expect(spy).toHaveBeenCalled();
+        expect(spy.calls.count()).toEqual(1);
+        expect(spy.calls.mostRecent().args[0]).toEqual(sampleOptions.key);
+        expect(spy.calls.mostRecent().args[1]).toEqual('error');
+        var arg2 = spy.calls.mostRecent().args[2],
+            objectsAreEqual = _.isEqual(arg2, extractedException);
+        expect(objectsAreEqual).toBeTruthy();
+    });
+
+});
--- test/options/specJsLogExpand.js
+++ test/options/specJsLogExpand.js
@@ -1,4 +1,4 @@
-describe('JsLog', function () {
+describe('expands JSON', function () {
     var logger,
         sampleOptions = {};
 
@@ -10,7 +10,7 @@ describe('JsLog', function () {
         logger = null;
     });
 
-    it('expands JSON', function () {
+    it('with ordering', function () {
         var sourceJson = {"b": 1, "a": 2, "c": [1, 2]},
             expandedJson = logger.expand(sourceJson),
             stringifiedJson = JSON.stringify(expandedJson),
@@ -18,7 +18,7 @@ describe('JsLog', function () {
         expect(stringifiedJson).toEqual(expectedResult);
     });
 
-    it('expands JSON with circular references', function () {
+    it('with circular references', function () {
         var sourceJson = {"b": 1, "a": 2};
         sourceJson.c = sourceJson;
         var expandedJson = logger.expand(sourceJson),
@@ -26,5 +26,22 @@ describe('JsLog', function () {
             expectedResult = '{"a":2,"b":1,"c":"<circular ref>"}';
         expect(stringifiedJson).toEqual(expectedResult);
     });
+
+    it('with null value', function () {
+        var sourceJson = {"b": 1, "a": 2, "c": null},
+            expandedJson = logger.expand(sourceJson),
+            stringifiedJson = JSON.stringify(expandedJson),
+            expectedResult = '{"a":2,"b":1,"c":null}';
+        expect(stringifiedJson).toEqual(expectedResult);
+    });
+
+    xit('and cuts functions', function () {
+        var sourceJson = {"b": 1, "a": 2, "f": function(){}},
+            expandedJson = logger.expand(sourceJson),
+            stringifiedJson = JSON.stringify(expandedJson),
+            expectedResult = '{"a":2,"b":1}';
+        expect(stringifiedJson).toEqual(expectedResult);
+    });
+
 
 });
